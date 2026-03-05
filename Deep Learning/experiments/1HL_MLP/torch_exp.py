05a42b078655c1b740e19dcc389b03d7e4a756bc
JimmyK300 <chiminh75@gmail.com>
2026-03-05 07:52:27 +0700

    5/3/2026

--- Deep Learning/experiments/1HL_MLP/torch_exp.py
+++ Deep Learning/experiments/1HL_MLP/torch_exp.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from torch.utils.data import DataLoader
-from torch.utils.data import Subset
+from torch.utils.data import DataLoader, Subset
 from torchvision import datasets, transforms
+import torch.nn.functional as F
 
 transform = transforms.ToTensor()
 
@@ -78,6 +78,34 @@ class ResMLP(nn.Module):
         x = self.output(x)
         return x
 
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        self.fc1 = nn.Linear(128 * 5 * 5, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
+        
+        x = torch.flatten(x, 1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        
+        return x
+
 def evaluate(loader):
     model.eval()
     correct = 0
@@ -85,7 +113,7 @@ def evaluate(loader):
     
     with torch.no_grad():
         for images, labels in loader:
-            images = images.view(images.size(0), -1)
+            # images = images.view(images.size(0), -1)
             outputs = model(images)
             _, predicted = torch.max(outputs, 1)
             
@@ -94,38 +122,38 @@ def evaluate(loader):
     
     return correct / total
 
-for hi in [12]:
-    model = ResMLP(depth=hi)
+# for hi in [12]:
+model = SimpleCNN()
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.1)
-    
-    train_acc = []
-    
-    for epoch in range(10):
-        model.train()
-        total_loss = 0
-        total = 0
-        correct = 0
-        for images, labels in train_loader:
-            images = images.view(images.size(0), -1)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-            optimizer.zero_grad()        # clear old gradients
-            outputs = model(images)      # forward
-            loss = criterion(outputs, labels)
-            loss.backward()              # compute gradients
-            optimizer.step()             # update weights
-            
-            _, predicted = torch.max(outputs, 1)
-            
-            total_loss += loss.item()
-            
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-        train_acc.append(correct / total)
+train_acc = []
+
+for epoch in range(10):
+    model.train()
+    total_loss = 0
+    total = 0
+    correct = 0
+    for images, labels in train_loader:
+        # images = images.view(images.size(0), -1)
+
+        optimizer.zero_grad()        # clear old gradients
+        outputs = model(images)      # forward
+        loss = criterion(outputs, labels)
+        loss.backward()              # compute gradients
+        optimizer.step()             # update weights
         
-        print(f"Epoch {epoch}: loss = {total_loss:.4f}")
-    print("L bn resdiual:", hi)
-    print("Train accuracy:", train_acc[-1])
-    print("Test accuracy:", evaluate(test_loader))
-    print("Epoch to conv:",  train_acc)
+        _, predicted = torch.max(outputs, 1)
+        
+        total_loss += loss.item()
+        
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+    train_acc.append(correct / total)
+    
+    print(f"Epoch {epoch}: loss = {total_loss:.4f}")
+# print("L bn resdiual:", hi)
+print("Train accuracy:", train_acc[-1])
+print("Test accuracy:", evaluate(test_loader))
+print("Epoch to conv:",  train_acc)
